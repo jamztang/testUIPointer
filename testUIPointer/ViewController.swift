3ec0bb2a98aaa91882336585a8f0a68c8f8454f8
James Tang <james@goodnotesapp.com>
2020-04-01 10:31:27 +0800

    Initial Commit

---
+++ testUIPointer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testUIPointer
+//
+//  Created by James Tang on 1/4/2020.
+//  Copyright © 2020 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
