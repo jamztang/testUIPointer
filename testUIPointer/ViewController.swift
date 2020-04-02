3503096dbe82a280df23e6cb0a61d6b5bfa1859a
James Tang <james@goodnotesapp.com>
2020-04-02 17:08:41 +0800

    Add horizontal/vertical beam style

--- testUIPointer/ViewController.swift
+++ testUIPointer/ViewController.swift
@@ -13,14 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var liftButton: UIButton!
     @IBOutlet weak var highlightButton: UIButton!
     @IBOutlet weak var hoverButton: UIButton!
+    @IBOutlet weak var horizontalBeamButton: UIButton!
+    @IBOutlet weak var verticalBeamButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        if #available(macCatalyst 13.4, *) {
+        if #available(macCatalyst 13.4, iOS 13.4, *) {
             liftButton.pointerStyleProvider = liftProvider
             hoverButton.pointerStyleProvider = hoverProvider
             highlightButton.pointerStyleProvider = highlightProvider
+            verticalBeamButton.pointerStyleProvider = verticalBeamProvider
+            horizontalBeamButton.pointerStyleProvider = horizontalBeamProvider
         } else {
             // Fallback on earlier versions
         }
@@ -28,7 +32,7 @@ class ViewController: UIViewController {
 
 }
 
-@available(macCatalyst 13.4, *)
+@available(macCatalyst 13.4, iOS 13.4, *)
 extension ViewController {
     func liftProvider(button: UIButton, effect: UIPointerEffect, shape: UIPointerShape) -> UIPointerStyle? {
         let style = UIPointerStyle(effect: .lift(.init(view: button)))
@@ -40,6 +44,14 @@ extension ViewController {
     }
     func hoverProvider(button: UIButton, effect: UIPointerEffect, shape: UIPointerShape) -> UIPointerStyle? {
         let style = UIPointerStyle(effect: .hover(.init(view: button), preferredTintMode: .overlay, prefersShadow: true, prefersScaledContent: true))
+        return style
+    }
+    func horizontalBeamProvider(button: UIButton, effect: UIPointerEffect, shape: UIPointerShape) -> UIPointerStyle? {
+        let style = UIPointerStyle(shape: .horizontalBeam(length: button.frame.size.height))
+        return style
+    }
+    func verticalBeamProvider(button: UIButton, effect: UIPointerEffect, shape: UIPointerShape) -> UIPointerStyle? {
+        let style = UIPointerStyle(shape: .verticalBeam(length: button.frame.size.height))
         return style
     }
 }
